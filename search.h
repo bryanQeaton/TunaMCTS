35edd40a0fa2e12b974c91ccb8e38655463b2b2e
bryan <Bryaneaton529@gmail.com>
2026-05-09 18:40:52 -0700

    improvement

--- search.h
+++ search.h
@@ -15,24 +15,30 @@ inline void score_moves(const chess::Board &pos,chess::Movelist &moves) {
             move.setScore(move.score()+10000);
         }
         move.setScore(move.score()+10000);
+        //psqt
+         if (pos.sideToMove()==chess::Color::WHITE) {
+             move.setScore(move.score()+mg_tables[static_cast<int>(pos.at(move.from()).type().internal())][flip(move.to().index())]-mg_tables[static_cast<int>(pos.at(move.from()).type().internal())][flip(move.from().index())]);
+         }
+         else {
+             move.setScore(move.score()+mg_tables[static_cast<int>(pos.at(move.from()).type().internal())][move.to().index()]-mg_tables[static_cast<int>(pos.at(move.from()).type().internal())][move.from().index()]);
+         }
     }
 }
 inline std::vector<float> policy(const chess::Movelist &moves) {
-    std::vector<float> po(moves.size(),0.f);
+    std::vector po(moves.size(),0.f);
     float sum=0;
     for (auto move:moves){sum+=move.score();}
     for (int i=0;i<po.size();i++){po[i]=moves[i].score()/sum;}
     return po;
 }
 
-inline Game_Tree::Node::Child uct_best(const float C,chess::Board &pos,const chess::Movelist &moves,const Game_Tree::Node &node) {
+inline Child uct_best(const float C,chess::Board &pos,const chess::Movelist &moves,const Node &node) {
     auto best_child=node.children[0];
     float best_uct=-INFINITY;
-
     for (const auto &child:node.children) {
         const auto &child_node=game_tree[child.idx];
-        const float wr=child_node.visits? 1.f-child_node.winrate(): 0.5f;
-        float uct=wr+C*child.prior*sqrtf(node.visits)/(1.f+child_node.visits);
+        float wr=child_node.visits? 1.f-child_node.winrate(): 0.5f;
+        const float uct=wr+C*child.prior*sqrtf(node.visits)/(1.f+child_node.visits);
         if (uct>best_uct) {
             best_uct=uct;
             best_child=child;
@@ -40,49 +46,43 @@ inline Game_Tree::Node::Child uct_best(const float C,chess::Board &pos,const che
     }
     return best_child;
 }
-inline float search(const float puct,chess::Board &pos,const int playout_maxdepth=100,const int idx=0) {
-    auto moves=chess::Movelist();
-    chess::movegen::legalmoves(moves,pos);
-    if (moves.empty()) {return pos.inCheck()? 0.0f: 0.5f;}
-    if (pos.isInsufficientMaterial()||pos.isHalfMoveDraw()||pos.isRepetition(1)) {return 0.5f;}
+inline float search(const float puct,chess::Board &pos,const int playout_maxdepth=100,const uint64_t idx=0) {
     if (game_tree.empty()) {//if tree is empty
         const float value=playout(pos,playout_maxdepth);
         game_tree.add(value,1,pos.hash());
         return value;
     }
-    assert(!game_tree.empty()&&"Game tree is empty!");
-    assert(game_tree[idx].hash==pos.hash()&&"Position hash doesn't match tree stored hash!");
-    // if (game_tree[idx].child_count()!=moves.size()) {
-    //     //expand
-    //     const auto child_move=moves[game_tree[idx].child_count()];
-    //     game_tree.add_child_to_node(child_move,game_tree.size(),0,idx);
-    //     //playout
-    //     pos.makeMove(child_move);
-    //     float value=playout(pos,playout_maxdepth);
-    //     uint64_t child_hash=pos.hash();
-    //     pos.unmakeMove(child_move);
-    //     game_tree.add(value,1,child_hash);
-    //     value=1.f-value;
-    //     game_tree.update_node(value,idx);
-    //     return value;
-    // }
+    auto moves=chess::Movelist();
+    chess::movegen::legalmoves(moves,pos);
+    if (moves.empty()) {
+        const float value=pos.inCheck()? 0.0f: 0.5f;
+        game_tree.update_node(value,idx);
+        return value;
+    }
+    if (idx!=0&&(pos.isInsufficientMaterial()||pos.isHalfMoveDraw()||pos.isRepetition(1))) {
+        game_tree.update_node(0.5f,idx);
+        return 0.5f;
+    }
+    // if (idx>=game_tree.size()) {throw std::runtime_error("search(): node index out of bounds");}
+    // if (game_tree[idx].hash!=pos.hash()){throw std::runtime_error("Hash difference!");}
+    // if (game_tree.empty()){throw std::runtime_error("Empty Tree!");}
     if (game_tree[idx].child_count()==0) {
         score_moves(pos,moves);
         const auto pol=policy(moves);
-        int best_idx=0;
+        uint64_t best_idx=0;
         float best_policy=-INFINITY;
         for (int m=0;m<moves.size();m++) {
-            const int child_idx=game_tree.size();
+            const uint64_t child_idx=game_tree.size();
+            game_tree.add(
+                0.5f,
+                0,
+                pos.zobristAfter(moves[m])
+            );
             game_tree.add_child_to_node(
                 moves[m],
                 child_idx,
                 pol[m],
                 idx
-            );
-            game_tree.add(
-                0,
-                0,
-                pos.zobristAfter(moves[m])
             );
             if (pol[m]>best_policy) {
                 best_policy=pol[m];
